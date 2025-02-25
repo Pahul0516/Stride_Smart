179aa26f387bb84a5a69bc0d95b9ea2268856cd1
berin <berindeiepaul8@gmail.com>
2025-02-25 18:14:41 +0200

    commit

--- backend/app/api/Frontend/front.py
+++ backend/app/api/Frontend/front.py
@@ -4,18 +4,18 @@ from flask import Blueprint
 
 frontend_bp =  Blueprint('app', __name__)
 
-@frontend_bp.route('/')
+@frontend_bp.route('/projects/2')
 def home():
     return render_template('index.html') 
 
-@frontend_bp.route('/login')
+@frontend_bp.route('/projects/2/login')
 def login():
     return render_template('login.html') 
 
-@frontend_bp.route('/map')
+@frontend_bp.route('/projects/2/map')
 def map_page():
     return render_template('map.html') 
 
-@frontend_bp.route('/register')
+@frontend_bp.route('/projects/2/register')
 def register():
     return render_template('register.html') 
